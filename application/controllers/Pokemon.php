88fff052eecad5969f5b261cf23841fe3ed47d92
harionofxx <harionofx7@gmail.com>
2016-10-11 04:20:04 +0200

    Update Pokemon.php

--- application/controllers/Pokemon.php
+++ application/controllers/Pokemon.php
@@ -15,7 +15,7 @@ class Pokemon_model extends CI_Model{
         return $this->db->get_where('pokemon',array('id' => $id))->result();
     }
     /* Fungsi ini menginsertkan data ke dalam tabel pokemon.
-     * Parameter $data berisi data yang akan diinsertkan dalam bentuk associative array
+     * Parameter $data berisi data yang akan di insertkan dalam bentuk associative array
      * yang terdiri dari pasangan key=>value. Key adalan nama field, sedangkan value adalah
      * nilai field
      */
@@ -24,7 +24,7 @@ class Pokemon_model extends CI_Model{
         $this->db->insert('pokemon', $data);
     }
     /* Fungsi ini mengupdate data ke dalam tabel pokemon yang memiliki id=$id.
-     * Parameter $data berisi data yang akan diinsertkan dalam bentuk associative array
+     * Parameter $data berisi data yang akan di insertkan dalam bentuk associative array
      * yang terdiri dari pasangan key=>value. Key adalan nama field, sedangkan value adalah
      * nilai field
      */
@@ -33,7 +33,7 @@ class Pokemon_model extends CI_Model{
         $this->db->where('id', $id);
         $this->db->update('pokemon', $data);
     }
-    /* Fungsi ini menghapus satu record dari tabel pokemon yan memiliki id=$id*/
+    /* Fungsi ini menghapus satu record dari tabel pokemon yang memiliki id=$id*/
     public function delete($id){
         // TODO: Modif bagian ini
         $this->db->delete('pokemon', array('id' => $id));
